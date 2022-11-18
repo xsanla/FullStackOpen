cf0547705b499ec48f667620e4f0329fa02bb884
Antti Laiho <97844941+xsanla@users.noreply.github.com>
2022-11-18 11:09:24 +0200

    tehtävät 2.11-2.14

--- osa2/maiden/src/App.js
+++ osa2/maiden/src/App.js
@@ -2,14 +2,51 @@ import axios from 'axios'
 import { useEffect, useState } from 'react'
 import Filter from './components/Filter'
 
-const Countries = (props) => {
-  const countriesToShow = props.countries
-  if (props.show == false && countriesToShow.length < 10 && countriesToShow.length !=1) {
+const Weather = (props) => {
+  const [weather, setWeather] = useState()
+  const [weatherIcon, setWeatherIcon] = useState()
+
+  useEffect(() => {
+    const urlEnd = 'lat=' + props.lat + '&lon=' + props.lon + '&appid=' + props.api_key + '&units=metric'
+    axios
+      .get('https://api.openweathermap.org/data/2.5/weather?' + urlEnd)
+      .then(response => {
+        setWeather(response.data)
+      })
+  }, [])
+
+  useEffect(() => {
+    if (weather != undefined) {
+      setWeatherIcon('http://openweathermap.org/img/wn/' + weather.weather.at(0).icon + '@2x.png')
+    }
+  }, [weather])
+
+  if (weather != undefined) {
     return (
       <div>
-        {countriesToShow.map((country) =>
-          <p key={country.name.official}>{country.name.official}</p>
-        )}
+        <h2>Weather in {props.capital}</h2>
+        <p>temperature {weather.main.temp} Celsius</p>
+        <img src={weatherIcon} alt="" />
+        <p>wind {weather.wind.speed} m/s</p>
+      </div>
+    )
+  }
+}
+
+const Countries = (props) => {
+  const countriesToShow = props.countries
+  const setFilter = props.setFilter
+  if (props.show == false && countriesToShow.length < 10 && countriesToShow.length != 1) {
+    return (
+      <div>
+        {countriesToShow.map((country) => {
+          return (
+            <div>
+              {country.name.official}
+              <button onClick={() => setFilter(country.name.official)}>show</button>
+            </div>
+          )
+        })}
       </div>
     )
   } else if (countriesToShow.length > 10) {
@@ -18,22 +55,27 @@ const Countries = (props) => {
         <p>Too many matches, specify another filter</p>
       </div>
     )
-  } else if(countriesToShow.length === 1) {
+  } else if (countriesToShow.length === 1) {
     const country = countriesToShow.at(0)
-    console.log("yks country jäljel")
-    console.log(country)
     return (
       <div>
         <h1>{country.name.official}</h1>
         <div>
-          capital {country.capital}
-          area {country.area}
+          <p>capital {country.capital}</p>
+          <p>area {country.area}</p>
         </div>
         <h2>languages</h2>
         <div>
-          {country.languages.map((language) =>
-            <p key={language}>{language}</p>
+          {Object.values(country.languages).map((language) =>
+            <p key={language}> {language}</p>
           )}
+        </div>
+        <div>
+          <img src={country.flags.png} alt="" />
+        </div>
+        <div>
+          <Weather lat={country.capitalInfo.latlng.at(0)} lon={country.capitalInfo.latlng.at(1)}
+            api_key={props.api_key} capital={country.capital.at(0)} />
         </div>
       </div>
     )
@@ -44,11 +86,11 @@ const App = () => {
   const [countries, setCountries] = useState([])
   const [filter, setFilter] = useState('')
   const [show, setShow] = useState(true)
+  const api_key = process.env.REACT_APP_API_KEY
 
   useEffect(() => {
     axios
       .get('https://restcountries.com/v3.1/all').then(response => {
-        console.log(response.data)
         setCountries(response.data)
       })
   }, [])
@@ -57,7 +99,6 @@ const App = () => {
     if (event.target.value.trim().length !== 0) {
       setFilter(event.target.value)
       setShow(false)
-      console.log("mitä vittua")
     } else {
       setShow(true)
       setFilter("")
@@ -71,7 +112,7 @@ const App = () => {
   return (
     <div>
       <Filter filter={filter} handleFilterChange={handleFilterChange} />
-      <Countries show={show} countries={countriesToShow} />
+      <Countries api_key={api_key} show={show} countries={countriesToShow} setFilter={setFilter} />
     </div>
   )
 }
