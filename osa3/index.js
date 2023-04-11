175940c7d50a824490735482ed8a28e34e8bd66a
Antti Laiho <97844941+xsanla@users.noreply.github.com>
2023-04-11 18:30:12 +0300

    osa3.2 tehty

---
+++ osa3/index.js
@@ -0,0 +1,47 @@
+const http = require('http')
+
+const express = require('express')
+const { time } = require('console')
+const app = express()
+
+let persons = [
+    { 
+        "name": "Arto Hellas", 
+        "number": "040-123456",
+        "id": 1
+      },
+      { 
+        "name": "Ada Lovelace", 
+        "number": "39-44-5323523",
+        "id": 2
+      },
+      { 
+        "name": "Dan Abramov", 
+        "number": "12-43-234345",
+        "id": 3
+      },
+      { 
+        "name": "Mary Poppendieck", 
+        "number": "39-23-6423122",
+        "id": 4
+      }
+]
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/persons', (req, res) => {
+  res.json(persons)
+})
+
+app.get('/info', (req, res) => {
+  const d = new Date
+  res.send(`<p1> Phonebook has info for ${persons.length} people<br>${d}`)
+  })
+
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
